397a475613f1e31126b425d0d884fa0a34d53c34
whipbaek <jibaek0513@gmail.com>
2022-11-22 15:42:44 +0900

    HW

---
+++ 10th Seam Carving/temp.py
@@ -0,0 +1,11 @@
+height = 4
+width = 5
+val = 0
+my2DList = [[0 for j in range(width)] for i in range(height)]
+print(my2DList)
+my2DList[0][1] = 5
+print(my2DList)
+
+myrlist = [1,2,3,4,5]
+myrlist.reverse()
+print(myrlist)
